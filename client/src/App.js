357bbeb25524dfe8df75d447594c3f967fd9ec4c
sashankshukla <ayaz.shukla@gmail.com>
2025-01-18 15:10:54 -0800

    install tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+// Components
+const Contacts = () => <h2>Contacts Page</h2>;
+const Calendar = () => <h2>Calendar Page</h2>;
+const Agents = () => <h2>Agents Page</h2>;
+const Dashboards = () => <h2>Dashboards Page</h2>;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-blue-400">adsfds</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/contacts">Contacts</Link>
+              </li>
+              <li>
+                <Link to="/calendar">Calendar</Link>
+              </li>
+              <li>
+                <Link to="/agents">Agents</Link>
+              </li>
+              <li>
+                <Link to="/dashboards">Dashboards</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* Routes */}
+          <Routes>
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/agents" element={<Agents />} />
+            <Route path="/dashboards" element={<Dashboards />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
